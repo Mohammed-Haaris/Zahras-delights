aa6acef5c386cfe303511da3a175e5a0da4ad63e
Mohammed-Haaris-Solvv <haaris.solvvtech@gmail.com>
2026-04-23 06:46:39 +0530

    installed packages

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return <> </>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @format */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
